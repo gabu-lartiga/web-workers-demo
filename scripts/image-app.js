ec98e98178f40ee88919ae92eaa793b74e27adf3
Gabu Lártiga <gabilartiga@gmail.com>
2016-03-22 16:20:14 -0300

    refactor: add wroker, has a bug

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -1,10 +1,13 @@
 (function(){
+
   // http://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
   var original;
   var imageLoader = document.querySelector('#imageLoader');
   imageLoader.addEventListener('change', handleImage, false);
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
+
+  var worker = new Worker('scripts/worker.js');
 
   function handleImage(e){
     var reader = new FileReader();
@@ -37,26 +40,23 @@
   function manipulateImage(type) {
     var a, b, g, i, imageData, j, length, pixel, r, ref;
     imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
     toggleButtonsAbledness();
+    worker.postMessage({'imageData': imageData, 'type': type});
 
-    // Hint! This is where you should post messages to the web worker and
-    // receive messages from the web worker.
-
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
+    worker.onmessage = function (e) {
+      console.log('passing right here');
+      toggleButtonsAbledness();
+      var image = e.data;
+      if(image) return ctx.putImageData(e.data, 0, 0);
+      console.log("No manipulated img returned.");
     }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+    // worker.onerror = function(error) {
+    //   function WorkerException(message) {
+    //     this.name = "WorkerException";
+    //     this.message = message;
+    //   };
+    //   throw new WorkerException('Worker error.');
+    // };
   };
 
   function revertImage() {
